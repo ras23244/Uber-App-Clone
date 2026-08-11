84d503789ca6020017623a6c075ee2e2b669c49e
231b260 <231b260@juetguna.in>
2026-08-11 23:51:36 +0530

    comments removed

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,32 +1,4 @@
-// import React from 'react'
 
-// const LocationSearchPanel = (props) => {
-//     const { suggestions, setPickup, setDestination, isPickup, setPickupCoords, setDestinationCoords, setPanelOpen } = props;
-
-//     return (
-//         <div>
-//             {
-//                 suggestions.map((location, idx) => {
-//                     return <div key={idx} onClick={() => {
-//                         if (isPickup) {
-//                             setPickup(location.description);
-//                             if (setPickupCoords) setPickupCoords({ lat: location.lat, lng: location.lng });
-//                         } else {
-//                             setDestination(location.description);
-//                             if (setDestinationCoords) setDestinationCoords({ lat: location.lat, lng: location.lng });
-//                         }
-//                         if (setPanelOpen) setPanelOpen(false);
-//                     }} className='flex items-center border-2 border-gray-300 active:border-black p-3 rounded-xl justify-start gap-4 my-4'>
-//                         <h2 className='bg-[#eee] h-10 w-14 flex items-center justify-center rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
-//                         <h4 className='font-medium'>{location.description}</h4>
-//                     </div>
-//                 })
-//             }
-//         </div>
-//     )
-// }
-
-// export default LocationSearchPanel
 import React from 'react';
 
 const LocationSearchPanel = (props) => {
