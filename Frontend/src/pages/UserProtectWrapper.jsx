add07d99832a3a9ef693a3ddc4391af9a11d4ce9
ras23244 <146091681+ras23244@users.noreply.github.com>
2026-08-12 11:32:45 +0530

    Fix import path for UserDataContext

--- Frontend/src/pages/UserProtectWrapper.jsx
+++ Frontend/src/pages/UserProtectWrapper.jsx
@@ -1,7 +1,7 @@
 import React, { useContext,useEffect,useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import { UserDataContext } from '../context/userContext';
+import { UserDataContext } from '../context/UserContext';
 
 
 const UserProtectWrapper = ({ children }) => {
